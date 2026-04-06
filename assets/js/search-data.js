c4977820b063c55cf0712c12e768fdd007b94d8d
kajari140309 <kajari@ucsb.edu>
2026-04-06 05:48:23 +0000

    Deploying to gh-pages from @ kajari140309/kajari140309.github.io@d3226fdfe1d63cf27db740f806fe37d4da7a4106 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -11,7 +11,7 @@ ninja.data = [{
     },
   },{id: "nav-research",
           title: "Research",
-          description: "A growing collection of your cool projects.",
+          description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/projects/";
